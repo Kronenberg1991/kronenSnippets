eefabc4982669db6948d707f899beb0fd0ddf5d1
Michael Kronenberg <kronenberg1991@gmail.com>
2016-03-27 15:57:50 +0300

    ultimate

---
+++ ultimate/alert.js
@@ -0,0 +1,536 @@
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+// INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS // INSTA FOLLOWS 
+   
+      var it = 0;
+    
+      function twitterFollow() {
+        it++;
+        if (it % 15 == 0) window.scrollTo(0,1000000000000000000000);
+         $(".user-actions-follow-button").get(it).click();
+      }
+      function startTwitterFollow() {
+        console.log('STARTING FOLLOW ON TWITTER!');
+        setInterval(twitterFollow, 600);
+      }
+    
+
+// insta follow
+var ii = 0;
+var count = 0;
+
+function click_follow(){
+    if (2 + 2 == 4) { // :)
+        $($("._jvpff").get(0)).click();
+        count++;
+        setTimeout(startsstartFollow, 2000);
+    } else {
+        console.log("skipping");
+       setTimeout(startsstartFollow, 2000);
+    }
+      console.log("done", ii, "count", count);
+}
+
+
+function startsstartFollow() {
+    window.scrollTo(0,0);
+    ii++;
+    if (ii%10 == 0){ window.scrollTo(0,1000000000000000000000) }
+    
+    $("a").get(ii).click();
+    setTimeout(click_follow, 2000);
+
+}
+
+function startInstaFollow() {
+    $('._oidfu').get(0).click();
+    setTimeout(startsstartFollow, 2000);
+}
+ 
+ var it = 0;
+    
+      function twitterFollow() {
+        it++;
+        if (it % 10 == 0) window.scrollTo(0,1000000000000000000000);
+         $(".user-actions-follow-button").get(it).click();
+      }
+      function startTwitterFollow() {
+        console.log('STARTING FOLLOW ON TWITTER!');
+        setInterval(twitterFollow, 600);
+      }
+    
+
+// INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES 
+// INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES 
+// INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES // INSTA LIKES 
+var il = 0;
+
+
+function click_like(){
+    if (2 + 2 == 4) { // :)
+        $('._ebwb5').get(0).click()
+        console.log('LIKE THIS MOTHERFUCKER!');
+        setTimeout(startstartLikes, 3500);
+    } else {
+        console.log("wtf bull shit?");
+        startstartLikes();
+    }
+
+}
+
+
+function startstartLikes() {
+    window.scrollTo(0,0);
+    il++;
+    if (il%10 == 0){
+     window.scrollTo(0,1000000000000000000000)
+   
+ }
+    
+    $("a").get(il).click();
+    setTimeout(click_like, 3500);
+
+}
+
+function startInstaLikes() {
+    $('._oidfu').get(0).click();
+    setTimeout(startstartLikes, 2000)
+}
+
+
+
+
+    
+
+
+  // TWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGER
+  // TWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGER
+  // TWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGERTWITTER MESSAGER
+
+function addMessageToTheTwitter() {
+  var jq = document.createElement('textarea');
+  $(jq).addClass('blabla');
+  $('textarea').css({"z-index": 1000000, "position":"absolute","top":50, "right":50});
+  document.getElementsByTagName('body')[0].appendChild(jq);
+  console.log('Input creating!');
+}
+
+
+var i = 1;
+var checkMessage = 1;
+var arrayCounter = -1;
+
+function typeNextOne(){
+    $('.tweet-action').get(0).click();
+         setTimeout(typeTheMessage, 5000);
+
+        function typeTheMessage() {
+            switch(checkMessage) {
+                case 1:
+                var textMessage = $($('.blabla').get(0)).val()
+                  checkMessage++;
+                  // HERE U CAN CHANGE THE MESSAGE
+                  $('#tweet-box-global div').text('Hello ' + $('.ProfileHeaderCard-name a').text() + textMessage);
+                  $('#tweet-box-global').focus();
+                  setTimeout(TWIT, 4000);
+                   function TWIT() {
+                  $('.tweet-btn').click();
+                  setTimeout(historyGo, 4000);
+                  function historyGo() {
+                    window.history.go(-1);
+                    setTimeout(START_TYPING, 3000);
+                  }      
+            }
+                  console.log('TYPE MESSAGE!!!');
+                  break;
+                  case 2: 
+                  var textMessage = $($('.blabla').get(0)).val()
+                  checkMessage++;
+                   // HERE U CAN CHANGE THE MESSAGE
+                   $('#tweet-box-global div').text('Hello ' + $('.ProfileHeaderCard-name a').text() + textMessage);
+                   $('#tweet-box-global').focus();
+                  setTimeout(TWIT, 4000);
+                   function TWIT() {
+                  $('.tweet-btn').click();
+                  setTimeout(historyGo, 4000);
+                  function historyGo() {
+                    window.history.go(-1);
+                    setTimeout(START_TYPING, 3000);
+                  }      
+            }
+                 console.log('TYPE MESSAGE!!!');
+                  break;
+                  case 3:
+                  var textMessage = $($('.blabla').get(0)).val()
+                  checkMessage++;
+                  checkMessage == 4 ? checkMessage = 1 : console.log('error');
+                   // HERE U CAN CHANGE THE MESSAGE
+                  $('#tweet-box-global div').text('Hello ' + $('.ProfileHeaderCard-name a').text() + textMessage);
+                  $('#tweet-box-global').focus();
+                  setTimeout(TWIT, 4000);
+                   function TWIT() {
+                  $('.tweet-btn').click();
+                  setTimeout(historyGo, 4000);
+                  function historyGo() {
+                    window.history.go(-1);
+                    setTimeout(START_TYPING, 3000);
+                  }      
+            }
+                  console.log('TYPE MESSAGE!!!');
+                  break;
+              }
+       }
+           
+       
+}
+
+
+function START_TYPING() {
+    window.scrollTo(0,0);
+    if (i%10 == 0) window.scrollTo(0,1000000000000000000000);
+     i++;
+     $('.ProfileCard-bg').get(i).click()
+
+     setTimeout(typeNextOne, 3500);
+      console.log(checkMessage + "IN OPEN NEXT FUNCTION!");
+
+}
+
+START_TYPING();
+
+   function stop() {
+      location.reload();
+    }
+
+
+
+    /*    .o                                .o88       .o88
+        .88                                "888       "888
+       o8888oo  ooo  oooo  ooo. .oo.  .oo.   888oooo.   888  oooo d8b
+       ""888""  888  "888  "888P"Y88bP"Y88b  d88' `88b  888  "888""8P
+         888    888   888   888   888   888  888   888  888   888
+         888 .  888   888   888   888   888  888.  888  888   888    .o.
+         "888Y  `V88V"V8P' o888o o888o o888o 88`bod8P' o888o d888b   Y8P 
+
+
+          8888888   88    88            8
+          88        88    88
+          8888888   88    88
+          88        88    88
+          88         .oooo.
+*/
+
+    // SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT 
+    // SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT 
+    // SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT SCRIPT 
+
+                    
+// FOLLOWING IN SLIDE BAR
+var tumblrIndex = -1;
+var scrollBar = 0;
+function runFollowOnTumblr() {
+  scrollBar++;
+  scrollBar % 2 == 0 ? $('.right').click()  : console.log('blablabla');
+  tumblrIndex++;
+  setTimeout(clickFollowButton, 2000)
+  function clickFollowButton() {
+    var buttonFollowText = $($('.header_controls .follow').get(tumblrIndex)).html();
+    if( buttonFollowText.indexOf("Follow") >= 0) {
+    $($('.header_controls .follow').get(tumblrIndex)).click();
+    console.log('Follow!');
+    setTimeout(runFollowOnTumblr, 2000);
+  } else {
+    console.log('already following!');
+    }
+  }
+}
+
+
+
+
+// UNFOLLOWING IN SLIDE BAR
+var tumblrIndextumblr = -1;
+var scrollBarBar = 0;
+function runUnFollowTumblr() {
+  scrollBarBar++;
+  scrollBarBar % 2 == 0 ?  $('.right').click() :  console.log('do some bullshit!');
+  tumblrIndextumblr++;
+  setTimeout(clickFollowButton, 1500)
+  function clickFollowButton() {
+    $($('.header_controls .unfollow').get(tumblrIndextumblr)).click();
+    setTimeout(UNFOLLOW, 1000) 
+    function UNFOLLOW() {
+      $('.ui_dialog .buttons .focus .ui_button').click();
+        setTimeout(runUnFollowTumblr, 2000);
+        console.log('CLICKING UNFOLLOW!');
+    }
+
+  }
+}
+
+
+
+
+// var indexIndex = 0;
+// function secondFollowing() {
+//   indexIndex++;
+//   $($('.tumblelog_avatar').get(indexIndex)).click();
+//   setTimeout(clickfollowplease, 1500)
+//   function clickfollowplease() {
+//     $('.header_controls .follow').click();
+//   }
+// }
+
+
+
+
+var tumblr = {}
+tumblr.counter = 0;
+
+tumblr.openUser = function() {
+  tumblr.counter++;
+  window.scrollTo(0,0);
+  if (tumblr.counter % 10 == 0) window.scrollTo(0,1000000000000000000000);
+  // $($('.tumblelog_avatar').get(tumblr.counter)).click();
+
+  $('.right').click();
+  console.log('BAR IS SCROLLING...');
+  setTimeout(sendMessageButton, 2555);
+  
+ 
+        function sendMessageButton() {
+           console.log('SEND MESSAGE BUTTON PRESSED...');
+          $($('.popover_inner ul li:nth-child(2)').get(tumblr.counter)).click();
+           setTimeout(typeTheMessage, 4444);
+          function typeTheMessage() {
+            console.log('Text editing...');
+             $('.text-input-container textarea').parent().next().removeAttr("disabled");
+            $('.text-input-container textarea').focus()
+    .typetype("Hello, How are you?! I made one cool thing, and share it in tumblr!!", {
+    callback: function() {
+      // $('body').addClass('reveal');
+       
+            
+
+    }
+  }).delay(350);
+
+
+
+             setTimeout(massageSended, 4444);
+             function massageSended() {
+              $('.text-input-container textarea').focus();
+              $('.text-input-conta')
+              $('.submit').click();
+              console.log('Send message!!')
+              setTimeout(tumblr.runScripts, 4444);
+             }
+          }
+
+        }
+
+  console.log(tumblr.counter); 
+}
+
+
+tumblr.runScripts = function() {
+  setTimeout(tumblr.openUser, 4000);
+}
+
+// RUN JQUERY FROM EXTENSION
+function runJquery() {
+  var jq = document.createElement('script');
+  jq.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js";
+  document.getElementsByTagName('head')[0].appendChild(jq);
+  console.log('JQUERY IN RUNNING!');
+}
+
+
+
+
+           // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+            // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+             // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+              // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+               // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                 // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                  // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                   // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                    // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                     // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                      // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                       // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                        // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                         // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+                          // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW // SMART UNFOLLOW
+
+
+
+                                                                         //  .88    
+                                                                         // o8888oo 
+                                                                         // ""888"" 
+                                                                         //   888   
+                                                                         //   888 . 
+                                                                         //   "888Y 
+
+
+var onButtonChecker = -1;
+var checkcheck = 1;
+var FOLLOWSTATUS = -1;
+var top = 10;
+function checkIfFollow() {
+     top+=25;
+     $(window).scrollTop(top) + " px";
+    if (checkcheck % 10 == 0) window.scrollTo(0,1000000000000000000000);
+     checkcheck++;
+     console.log(checkcheck);
+     onButtonChecker++;
+     console.log(onButtonChecker);
+
+     FOLLOWSTATUS++;
+     console.log(FOLLOWSTATUS);
+ 
+     setTimeout(checkFollowStatus, 777);
+      function checkFollowStatus() {
+        var x = $($('.ProfileCard-screenname').get(FOLLOWSTATUS)).find('span').text();
+
+     if(x.indexOf('follows you') > 0) {
+          console.log(x.indexOf('follows you') > 0);
+          console.log(x + '        FOLLOW YOU!');
+         
+        
+          
+        setTimeout(checkNextOne, 1500);
+     } else if(x.indexOf('follows you') < 0) {
+        console.log(x.indexOf('follows you') < 0);
+        console.log(x + '         DOESNT FOLLOW YOU!');
+        $($('.ProfileCard').get(onButtonChecker)).find('.user-actions-follow-button').click();
+       
+        setTimeout(checkNextOne, 1500);
+     } else {
+      console.log('FINISH!');
+     }
+  }
+}
+
+function checkNextOne() {
+  setTimeout(checkIfFollow, 1500);
+}
+
+
+
+
+
+// $.ajax({
+//   method: "POST",
+//   url: "/svc/conversations/messages",
+//   data: { "content[text]":"dadasdasdasdasd",
+// "post[type]": "",
+// "post[summary]": "",
+// "post[state]": "",
+// "hasAnimation":true,
+// "clientTs":"Wed Mar 23 2016 14:37:37 GMT+0200 (IST)",
+// "isPending":true,
+// "type":"TEXT",
+// "message":"dadasdasdasdasd",
+// "ts": "",
+// "participant":"kronenberg1991.tumblr.com",
+// "collapseGroup":"new",
+// "tsHeader": "",
+// "isError":false,
+// "isCollapsed":true,
+// "canRetry":true,
+// "postHasTextMessage":false,
+// "context": "",
+// "conversation_id":24020802 }
+// })
+//       $.ajax({
+//         method: 'POST',
+//         url: '/svc/conversations/messages',
+//         headers: {
+//           "x-tumblr-form-key":"WAASOCHzuX7BJsmbeZQ48SKiR7w"
+                              
+//       },
+//         data: { "content[text]":"ANDREY EBAT EBAT! :D",
+//   "post[type]": "",
+//   "post[summary]": "",
+//   "post[state]": "",
+//   "hasAnimation":true,
+//   "clientTs":"Wed Mar 23 2016 14:37:37 GMT+0200 (IST)",
+//   "isPending":true,
+//   "type":"TEXT",
+//   "message":"HELLO U HOW ARE U?",
+//   "ts": "",
+//   "participant":"kronenberg1991.tumblr.com",
+//   "collapseGroup":"new",
+//   "tsHeader": "",
+//   "isError":false,
+//   "isCollapsed":true,
+//   "canRetry":true,
+//   "postHasTextMessage":false,
+//   "context": "",
+//   "conversation_id":24086252 },
+                   
+//       success: function(data, textStatus, request, xhr){
+//           alert(request.getAllResponseHeaders());
+
+//      },
+//      error: function (request, textStatus, errorThrown) {
+//           alert(request.getAllResponseHeaders());
+//      }
+//     });
+
+ 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+ $.ajax({
+        method: 'POST',
+        url: '/svc/conversations/messages',
+        headers: {
+          "x-tumblr-form-key":"WAASOCHzuX7BJsmbeZQ48SKiR7w"
+                              
+      },
+        data: { "content[text]":"Hello, What is your real name bro?",
+"post[type]":"",
+"post[summary]":"",
+"post[state]":"",
+"hasAnimation":"true",
+"clientTs":"Sun Mar 27 2016 10:58:41 GMT+0300 (IDT)",
+"isPending":true,
+"type":"TEXT",
+"message":"Hello, What is your real name bro?",
+"ts":"",
+"participant":"kronenberg1991.tumblr.com",
+"collapseGroup":"new",
+"tsHeader":"Today at 10:58 AM",
+"isError":false,
+"isCollapsed":false,
+"canRetry":true,
+"postHasTextMessage":false,
+"context":"",
+"participants[]":"kronenberg1991.tumblr.com",
+"participants[]":"ragecomics4you.tumblr.com",
+"conversation_id": getRandomInt( 23689180,24672147)
+
+},         
+      success: function(data, textStatus, request, xhr){
+          alert(request.getAllResponseHeaders());
+     },
+     error: function (request, textStatus, errorThrown) {
+          alert(request.getAllResponseHeaders());
+     }
+    });
+
+ 246 72 147
+ 236 89 180
+ 236 84 059
+ 246 72 349
+
---
+++ ultimate/popup.js
@@ -0,0 +1,91 @@
+
+
+function typeMessage() {
+    chrome.tabs.executeScript( null, {code: "START_TYPING()"},
+   function(results){ console.log(results); } );
+}
+
+function twitterFillow() {
+    chrome.tabs.executeScript( null, {code: "startTwitterFollow()"},
+   function(results){ console.log(results); } );
+}
+
+function instaFollow() {
+    chrome.tabs.executeScript( null, {code: "startInstaFollow()"},
+   function(results){ console.log(results); } );
+}
+
+function stop() {
+    chrome.tabs.executeScript( null, {code: "stop()"},
+   function(results){ console.log(results); } );
+}
+
+function tumblrTodo() {
+    chrome.tabs.executeScript( null, {code: "tumblr.runScripts()"},
+   function(results){ console.log(results); } );
+}
+
+function runJquery() {
+    chrome.tabs.executeScript( null, {code: "runJquery()"},
+   function(results){ console.log(results); } );
+}
+
+function checkUnfollow() {
+    chrome.tabs.executeScript( null, {code: "checkNextOne()"},
+   function(results){ console.log(results); } );
+}
+
+function instaLikes() {
+    chrome.tabs.executeScript( null, {code: "startInstaLikes()"},
+   function(results){ console.log(results); } );
+}
+
+function tumblrFollow() {
+    chrome.tabs.executeScript( null, {code: "runFollowOnTumblr()"},
+   function(results){ console.log(results); } );
+}
+
+
+function tumblrUfollow() {
+    chrome.tabs.executeScript( null, {code: "runUnFollowTumblr()"},
+   function(results){ console.log(results); } );
+}
+
+function addMessageToTheTwitter() {
+    chrome.tabs.executeScript( null, {code: "addMessageToTheTwitter()"},
+   function(results){ console.log(results); } );
+}
+
+
+document.getElementById('add-message').addEventListener('click', addMessageToTheTwitter);
+
+
+document.getElementById('tumblr-follow').addEventListener('click', tumblrFollow);
+document.getElementById('tumblr-unfollow').addEventListener('click', tumblrUfollow);
+
+document.getElementById('instagram-likes').addEventListener('click', instaLikes);
+
+document.getElementById('unfollow').addEventListener('click', checkUnfollow);
+
+document.getElementById('clickme').addEventListener('click', typeMessage);
+
+document.getElementById('twitter-follow').addEventListener('click', twitterFillow);
+
+document.getElementById('instagram-follow').addEventListener('click', instaFollow);
+
+document.getElementById('tumblr').addEventListener('click', tumblrTodo);
+
+document.getElementById('runjquery').addEventListener('click', runJquery);
+
+document.getElementById('stop').addEventListener('click', stop);
+
+var audio = $("#mySoundClip")[0];
+        $("button").mouseenter(function() {
+        audio.play();
+      });
+
+var audio2 = $("#secondSound")[0];
+        $("button").click(function() {
+        audio2.play();
+      });
+
